38ff0e51dfdae75d5b3c3dbefbfd20305e13b685
Antonio Horacio Rodrigues de Magalhaes <horaciomagalhaes7@gmail.com>
2023-03-05 18:56:05 -0300

    Adicionando maneira de compilar e executar códigos e organizando o repositório

--- codes/dot_product/dot_product.cpp
+++ codes/dot_product/dot_product.cpp
@@ -8,7 +8,7 @@ constexpr int n = 20;
 int incA = 1,incB = 1;
 
 int main(){
-    sycl :: queue qs {cpu_selector{}};
+    sycl :: queue qs {sycl :: cpu_selector{}};
     vector < int > A(n);
     vector < int > B(n);
 
@@ -17,13 +17,13 @@ int main(){
     sycl::buffer buffB{B};
     sycl::buffer < int, 1 > ans { sycl::range { 1 }};
     //submitting work in queue
-    qs.submit([&] (handler &h){
+    qs.submit([&] (sycl :: handler &h){
         // creating accessor
         sycl::accessor accA{buffA,h};
         sycl::accessor accB{buffB,h};
         h.parallel_for(sycl::range{n},[=] (sycl::id<1> idx){
             // filling vectors
-            int i = idx(0);
+            int i = idx[0];
             accA[i] = i * 3 + 1;
             accB[i] = i * 2 - 2;
         });
